96439d7ac5ff6ef6583fd7fa6a924935c9c537bf
PaulNorthern <sculltakers111@gmail.com>
2020-11-24 21:27:20 +0300

    Практика с componentDidMount

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,15 +1,46 @@
 import React, { Component } from "react";
-
+import Spinner from "../spinner";
+import SwapiService from "../../services/swapi-service";
 import "./item-list.css";
 
 export default class ItemList extends Component {
+  swapiService = new SwapiService();
+
+  state = {
+    peopleList: null,
+  };
+
+  componentDidMount() {
+    // лучшее место для вызова API, чем через constructor
+    // означает что компонент уже полностью проинициализирован
+    this.swapiService.getAllPeople().then((peopleList) => {
+      this.setState({ peopleList });
+    });
+  }
+
+  renderItems(arr) {
+    return arr.map(({ id, name }) => {
+      return (
+        <li
+          className="list-group-item"
+          key={id}
+          onClick={() => this.props.onItemSelected(id)}
+        >
+          {name}
+        </li>
+      );
+    });
+  }
+
   render() {
-    return (
-      <ul className="item-list list-group">
-        <li className="list-group-item">Luke Skywalker</li>
-        <li className="list-group-item">Darth Vader</li>
-        <li className="list-group-item">R2-D2</li>
-      </ul>
-    );
+    const { peopleList } = this.state;
+
+    if (!peopleList) {
+      return <Spinner />;
+    }
+
+    const items = this.renderItems(peopleList);
+
+    return <ul className="item-list list-group">{items}</ul>;
   }
 }
